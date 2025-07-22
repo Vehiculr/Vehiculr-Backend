a4ec810beb55379ce0a71303852f3f098ec5306b
Ashish Kumar <ashishpatidar.metaz@getvymo.com>
2025-07-23 01:43:51 +0530

    update folder structure

--- app/middleware/errorHandler.js
+++ app/middleware/errorHandler.js
@@ -1,9 +1,10 @@
 // app/middleware/errorHandler.js
-module.exports = function errorHandler(err, req, res, next) {
-    const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+module.exports = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
 
-    res.status(statusCode).json({
-        message: err.message || 'Internal Server Error',
-        stack: process.env.NODE_ENV === 'development' ? err.stack : undefined,
-    });
+  res.status(statusCode).json({
+    status: err.status || 'error',
+    message: err.message || 'Internal Server Error',
+    stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
+  });
 };
