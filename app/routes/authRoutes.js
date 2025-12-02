f2c0610222234b4db732458f065fef9391758722
Ashish Kumar <ashishpatidar.metaz@getvymo.com>
2025-12-02 16:13:38 +0530

    update code for vehicalType

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
